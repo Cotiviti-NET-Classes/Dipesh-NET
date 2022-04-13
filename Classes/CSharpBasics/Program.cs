c3c452978f95d513a1c3edddfa888ecc838d6d58
Dipesh Chapagain <hancymig33@gmail.com>
2022-04-13 15:21:46 +0545

    Generate Console Template.

---
+++ Classes/CSharpBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ABC
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
